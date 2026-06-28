0e1dacfb0c354504e0834748e7f0f04299c747c2
aifiles2026 <gabrielranmoi@gmail.com>
2026-06-28 17:19:35 -0600

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,46 @@
+// Service Worker - Menú Align (CocinArte CR)
+const CACHE_NAME = 'menu-align-v1';
+const ASSETS_TO_CACHE = [
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+// Instalación: guarda los archivos básicos en caché
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS_TO_CACHE))
+  );
+  self.skipWaiting();
+});
+
+// Activación: limpia cachés viejas de versiones anteriores
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+// Fetch: intenta red primero (para tener el menú actualizado),
+// si no hay internet usa la copia guardada en caché
+self.addEventListener('fetch', (event) => {
+  if (event.request.method !== 'GET') return;
+
+  event.respondWith(
+    fetch(event.request)
+      .then((response) => {
+        const responseClone = response.clone();
+        caches.open(CACHE_NAME).then((cache) => {
+          cache.put(event.request, responseClone);
+        });
+        return response;
+      })
+      .catch(() => caches.match(event.request))
+  );
+});
